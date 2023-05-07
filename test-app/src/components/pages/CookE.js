3cd59650082dfc73b49040725a570735507ef670
Yajurva Trivedi <yajurva.trivedi@ryerson.ca>
2023-05-07 01:44:42 -0400

    sorta

--- test-app/src/components/pages/CookE.js
+++ test-app/src/components/pages/CookE.js
@@ -1,55 +1,106 @@
-import React from 'react'
-import axios from "axios"
-import {useState} from "react"
-import { Button } from '../button/Button';
-import logo from '../images/cook-e.svg';
+import React from "react";
+import axios from "axios";
+import { useState } from "react";
+import { Button } from "../button/Button";
+import logo from "../images/cook-e.svg";
 import "./CookE.css";
 
 export default function CookE() {
-    const [prompt, setPrompt] = useState("");
-    const [response, setResponse] = useState("");
+  const [prompt, setPrompt] = useState("");
+  const [response, setResponse] = useState("");
+  const [recipe, setRecipe] = useState("");
+  const [materials, setMaterials] = useState("");
 
-    const handleSubmit = (e) =>{
-        e.preventDefault();
-        axios
-        .post("http://localhost:4001/chat", { prompt: prompt })
-        .then((res) => {
-            console.log(prompt);
-            setResponse(res.data);
-        })
-        .catch((err) =>{
-            console.error(err);
-        })
-    }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .post("http://localhost:4001/chat", { prompt: prompt })
+      .then((res) => {
+        console.log(prompt);
+        if (res.data.includes("Instructions:")) {
+            const ingredients = res.data.split("Instructions:")[0];
+            const directions = res.data.split("Instructions:")[1];
+            setMaterials(ingredients)
+            setRecipe(directions)
+
+            //console.log(ingredients + "\n\n\n\n\n" + directions);
+        }
+        if (res.data.includes("Directions:")) {
+            const ingredients = res.data.split("Instructions:")[0];
+            const directions = res.data.split("Instructions:")[1];
+            setMaterials(ingredients)
+            setRecipe(directions)
+
+            //console.log(ingredients + "\n\n\n\n\n" + directions);
+        }
+        if (res.data.includes("Recipe:")) {
+            const ingredients = res.data.split("Instructions:")[0];
+            const directions = res.data.split("Instructions:")[1];
+            setMaterials(ingredients)
+            setRecipe(directions)
+
+            //console.log(ingredients + "\n\n\n\n\n" + directions);
+        }
+        setResponse(res.data);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
+
   return (
-    <> 
-    <div className='cookeApp'>
-        <div className='appHeader'>
-            <img src={logo} alt="" style={{ width: "300px"}}/>
-            <h3 className='text-cookeapp'>get your favourite recipe in 10 seconds</h3>
+    <>
+      <div className="cookeApp">
+        <div className="appHeader">
+          <img src={logo} alt="" style={{ width: "300px" }} />
+          <h3 className="text-cookeapp">
+            get your favourite recipe in 5 seconds
+          </h3>
         </div>
-        <div className='prompt'>
-            <form className='form' onSubmit={handleSubmit}>
+        <div className="prompt">
+          <form className="form" onSubmit={handleSubmit}>
             <div>
-                <h3>Enter Prompt:</h3>
+              <h3>Enter Prompt:</h3>
             </div>
             <div>
-                <input  className='input'
-                placeholder='Enter the name of your meal'
-                type="text" 
-                value={prompt} 
-                onChange={(e) => setPrompt(e.target.value)}/>
+              <input
+                className="input"
+                placeholder="Enter the name of your meal"
+                type="text"
+                value={prompt}
+                onChange={(e) => setPrompt(e.target.value)}
+              />
             </div>
-            <Button type="submit" className="btn"  buttonStyle='btn-primary' buttonSize = 'btn-medium'buttonColor='dark'>
-                Submit
-            </Button>  
-            </form>
+            <Button
+              type="submit"
+              className="btn"
+              buttonStyle="btn-primary"
+              buttonSize="btn-medium"
+              buttonColor="dark"
+            >
+              Submit
+            </Button>
+          </form>
         </div>
-        <div className='response-container'>
-            <p className='resp'>{response}</p>
+        <div className="cards">
+            <div className="response-container">
+            <p className="resp">{response}</p>
+            </div>
+            <div className="response-container">
+            <p className="resp">{response}</p>
+            </div>
+            <div className="response-container">
+            <p className="resp">{response}</p>
+            </div>
         </div>
+      </div>
+      <div>
+        Materials OOOoooGAAABOOOOGAAA:
+        {materials}
 
-    </div>
+        Recipe OOOoooGAAABOOOOGAAA:
+        {recipe}
+      </div>
     </>
-  )
+  );
 }
