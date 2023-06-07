e3d2b2232d8058243128a32d0fa148dd7e034a08
Yajurva Trivedi <yajurva.trivedi@ryerson.ca>
2023-06-07 00:56:58 -0400

    cleanup backend

--- backend/chat.js
+++ backend/chat.js
@@ -1,4 +1,4 @@
-require('./app.js')
+require('./app.js');
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
@@ -8,7 +8,6 @@ const nutrition = require("./nutrition");
 require("dotenv").config();
 const apiKey = process.env.COHERE_API_KEY;
 
-// const openai = new OpenAIApi(config);
 const cohere = require("cohere-ai");
 cohere.init(apiKey);
 
@@ -17,45 +16,44 @@ if (fs.existsSync('history.txt')) {
 }
 fs.createWriteStream("history.txt");
 
-//setup Server
-const app = new express();
+const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
 let i = 0;
 
-function isFileEmpty(fileName, ignoreWhitespace=true) {
+function isFileEmpty(fileName, ignoreWhitespace = true) {
   return new Promise((resolve, reject) => {
-      fs.readFile(fileName, (err, data) => {
-          if( err ) {
-              reject(err);
-              return;
-          }
+    fs.readFile(fileName, (err, data) => {
+      if (err) {
+        reject(err);
+        return;
+      }
 
-          resolve((!ignoreWhitespace && data.length == 0) || (ignoreWhitespace && !!String(data).match(/^\s*$/)))
-      });
+      resolve((!ignoreWhitespace && data.length == 0) || (ignoreWhitespace && !!String(data).match(/^\s*$/)))
+    });
   })
 }
 
 app.delete("/history", (req, res) => {
-  fs.writeFile('./history.txt', '', function(){console.log('done')})
+  fs.writeFile('./history.txt', '', function () {
+    console.log('done');
+  });
   res.status(200).send("History deleted successfully!");
 });
 
-// apps.post()
 app.post("/chat", async (req, res) => {
   const prompt = req.body["prompt"];
   const message = fs.readFileSync("history.txt");
   console.log(prompt);
   let calories;
   let food;
-  isFileEmpty('history.txt')
-  .then( (isEmpty) => {
-    console.log( "empty:", isEmpty) // true or false
-  })
-  .catch( (err) => {
+  try {
+    const isEmpty = await isFileEmpty('history.txt');
+    console.log("empty:", isEmpty);
+  } catch (err) {
     i = 0;
-  });
+  }
   if (i < 1) {
     input = "Give me the ingredients list for " + prompt + " with few ingredients. Add measurements for each ingredient. Clearly label the ingredients and recipe.";
     food = prompt;
@@ -82,26 +80,21 @@ app.post("/chat", async (req, res) => {
     console.log('Saved!');
   });
 
-
-  nutrition.getCalories(food)
-    .then((result) => {
-      calories = result;
-      console.log(calories);
-      res.send({ recipe, calories });
-    })
-    .catch((err) => {
-      console.error(err);
-      res.send({ recipe, calories });
-    });
-  
+  try {
+    calories = await nutrition.getCalories(food);
+    console.log(calories);
+    res.send({ recipe, calories });
+  } catch (err) {
+    console.error(err);
+    res.send({ recipe, calories });
+  }
 
   console.log("<=====================RECIPE==================>");
-  console.log(recipe)
+  console.log(recipe);
   i++;
 });
 
-
-const port = 4001;
+const port = process.env.PORT || 4001;
 app.listen(port, () => {
   console.log(`server is listening on port ${port}`);
 });
